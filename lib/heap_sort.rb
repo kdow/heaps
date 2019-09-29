60063a38433abeb84767fbaa62c15322ecd5d41e
Kelly Downes <kedownes@gmail.com>
2019-09-29 09:30:12 -0700

    Complete heapsort method

--- lib/heap_sort.rb
+++ lib/heap_sort.rb
@@ -1,8 +1,20 @@
 
 
 # This method uses a heap to sort an array.
-# Time Complexity:  ?
-# Space Complexity: ?
-def heap_sort(list)
-  raise NotImplementedError, "Method not implemented yet..."
+# Time Complexity:  O(nlog n)
+# Space Complexity: O(1)
+def heapsort(list)
+  return list if list.length < 2
+
+  heap = MinHeap.new
+
+  until list.empty?
+    heap.add(list.pop)
+  end
+
+  until heap.empty?
+    list << heap.remove
+  end
+
+  return list
 end
